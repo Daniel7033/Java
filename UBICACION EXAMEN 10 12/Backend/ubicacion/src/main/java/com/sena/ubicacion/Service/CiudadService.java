021b6cf492943837be8d02475aeafe4bd90c82c2
Daniel7033 <j.audor7033@gmail.com>
2023-12-10 10:54:53 -0500

    Backend

---
+++ UBICACION EXAMEN 10 12/Backend/ubicacion/src/main/java/com/sena/ubicacion/Service/CiudadService.java
@@ -0,0 +1,75 @@
+package com.sena.ubicacion.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sena.ubicacion.Entity.Ciudad;
+import com.sena.ubicacion.IRepository.ICiudadRepository;
+import com.sena.ubicacion.IService.ICiudadService;
+
+@Service
+public class CiudadService implements ICiudadService{
+
+	@Autowired
+	private ICiudadRepository repository;
+	
+	@Override
+	public List<Ciudad> findAll() {
+		return repository.findAll();
+	}
+
+	@Override
+	public Optional<Ciudad> findById(Long id) {
+		return repository.findById(id);
+	}
+
+	@Override
+	public Ciudad save(Ciudad cliente) {
+		cliente.setFechaCreacion(LocalDateTime.now());
+		return repository.save(cliente);
+	}
+
+	@Override
+	public void update(Ciudad ciudad, Long id) {
+		//Consultar si existe el registro
+				Optional<Ciudad> op = repository.findById(id);
+				
+				if(op.isEmpty()){
+					System.out.print("Registro no existe.");
+				}else {
+					Ciudad updateCliente = op.get();					
+					updateCliente.setCodigo(ciudad.getCodigo());
+					updateCliente.setNombre(ciudad.getNombre());
+					updateCliente.setEstado(ciudad.getEstado());
+					updateCliente.setFechaModificacion(LocalDateTime.now());
+					
+					repository.save(updateCliente);
+				}
+	}
+	
+	@Override
+	public void deletedAt(Long id) {
+		//Consultar si existe el registro
+		Optional<Ciudad> op = repository.findById(id);
+		
+		if(op.isEmpty()){
+			System.out.print("Registro no existe para eliminar.");
+		}else {
+			Ciudad updateCiudad = op.get();					
+			updateCiudad.setFechaEliminacion(LocalDateTime.now());					
+			repository.save(updateCiudad);
+		}	
+		
+	}
+
+	@Override
+	public void delete(Long id) {
+		repository.deleteById(id);	
+	}
+
+}
+
